1b5897c7e29ef8ce538d9335859f09252cbca079
Lauramc13 <laura.morales8@alu.uclm.es>
2021-12-20 11:52:16 +0100

    testing with jacoco and surefire

---
+++ eclipse-workspace2/Testing/src/main/java/ISO2/Testing/Codigo.java
@@ -0,0 +1,93 @@
+package ISO2.Testing;
+
+public class Codigo {
+
+    public static int adecuacionFuncional(int comFun, int corFun, int perFun){
+    	//Tabla de completitud Funcional
+    	if(comFun >= 0 && comFun <10) comFun= 0;
+    	else if(comFun >= 10  && comFun <35) comFun= 1;
+    	else if(comFun >=  35 && comFun <70) comFun= 2;
+    	else if(comFun >= 70  && comFun <90) comFun= 3;
+    	else if(comFun >= 90  && comFun <=100) comFun= 4;
+    	else comFun = -1;
+
+    	//Tabla de correccion Funcional
+    	if(corFun >= 0  && corFun <10) corFun= 0;
+    	else if(corFun >= 10  && corFun <50) corFun= 1;
+    	else if(corFun >=  50 && corFun <70) corFun= 2;
+    	else if(corFun >= 70  && corFun <90) corFun= 3;
+    	else if(corFun >= 90  && corFun <=100) corFun= 5;
+    	else corFun = -1;
+
+    	//Tabla de pertinencia Funcional
+    	if(perFun >= 0  && perFun <10) perFun= 0;
+    	else if(perFun >= 10  && perFun <50) perFun= 2;
+    	else if(perFun >=  50 && perFun <70) perFun= 3;
+    	else if(perFun >= 70  && perFun <90) perFun= 4;
+    	else if(perFun >= 90  && perFun <=100) perFun= 5;
+    	else perFun = -1;
+
+    	int min = Math.min(Math.min(comFun, corFun), perFun);
+    	return min;
+
+    }
+    
+	public static int mantenibilidad(int mod, int reu, int ana, int capMod, int capProb){
+		//Tabla de modularidad
+		if(mod >= 0  && mod < 10) mod = 0;
+		else if(mod >= 10  && mod < 35) mod = 1;
+		else if(mod >= 35 && mod < 70) mod = 2;
+		else if(mod >= 70  && mod < 90) mod = 3;
+		else if(mod >= 90  && mod <= 100) mod = 4;
+		else mod = -1;
+
+		//Tabla de reusabilidad
+		if(reu >= 0  && reu < 10) reu = 0;
+		else if(reu >= 10  && reu < 35) reu = 1;
+		else if(reu >= 35 && reu < 70) reu = 2;
+		else if(reu >= 70  && reu < 90) reu = 3;
+		else if(reu >= 90  && reu <= 100) reu = 5;
+		else reu = -1;
+
+		//Tabla de analizabilidad
+		if(ana >= 0  && ana < 35) ana = 0;
+		else if(ana >= 35  && ana < 50) ana = 1;
+		else if(ana >= 50 && ana < 70) ana = 2;
+		else if(ana >= 70  && ana < 90) ana = 3;
+		else if(ana >= 90  && ana <= 100) ana = 5;
+		else ana = -1;
+
+		//Tabla de capacidad de ser modificado
+		if(capMod >= 0  && capMod < 10) capMod = 0;
+		else if(capMod >= 10  && capMod < 35) capMod = 1;
+		else if(capMod >= 35  && capMod < 50) capMod = 2;
+		else if(capMod >= 50 && capMod < 70) capMod = 3;
+		else if(capMod >= 70  && capMod < 90) capMod = 4;
+		else if(capMod >= 90  && capMod <= 100) capMod = 5;
+		else capMod = -1;
+
+		//Tabla de capacidad de ser probado
+		if(capProb >= 0  && capProb < 10) capProb = 0;
+		else if(capProb >= 10  && capProb < 50) capProb =1;
+		else if(capProb >= 50 && capProb < 70) capProb = 2;
+		else if(capProb >= 70  && capProb <= 100) capProb = 4;
+		else capProb = -1;
+
+		int min = Math.min(Math.min(mod, Math.min(reu, Math.min(ana, capMod))), capProb);
+		return min;
+	}
+	
+
+	public static int calidadGlobal(int man, int ade){
+
+		if((man >=1 && man<=5 && ade== 1) || (ade==2 && man == 1)) return 1;
+		else if((man >=2 && man<=5 && ade== 2) || (ade==3 && (man==1 || man== 2))) return 2;
+		else if((man >=3 && man<=5 && ade== 3) || (ade==4 && man>=1 && man<=4) || (ade == 5 && (man==1 || man== 2))) return 3;
+		else if((man==5 && ade== 4) || (ade==5 && (man==3 || man== 4))) return 4;
+		else if(man == 5 && ade == 5) return 5;
+		
+		return -1;
+
+	}
+
+}
---
+++ eclipse-workspace2/Testing/src/test/java/ISO2/Testing/CodigoTest.java
@@ -0,0 +1,199 @@
+package ISO2.Testing;
+//import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class CodigoTest {
+	
+	private Codigo c;
+	private Main m;
+
+	@Before
+	public void setUp() throws Exception {
+		c = new Codigo();
+		m = new Main();
+	}
+
+	//TESTS DE ADECUACION FUNCIONAL
+	@Test
+	public void testAdecuacionFuncional() {
+		assertEquals(-1, c.adecuacionFuncional(-5,-5,-5));
+	}
+	
+	@Test
+	public void testAdecuacionFuncional2() {
+		assertEquals(0, c.adecuacionFuncional(7,8,5));
+	}
+	
+	@Test
+	public void testAdecuacionFuncional3() {
+		assertEquals(1, c.adecuacionFuncional(10,10,10));
+	}
+	
+	@Test
+	public void testAdecuacionFuncional4() {
+		assertEquals(2, c.adecuacionFuncional(50,50,50));
+	}
+	
+	@Test
+	public void testAdecuacionFuncional5() {
+		assertEquals(3, c.adecuacionFuncional(70,70,70));
+	}
+	
+	@Test
+	public void testAdecuacionFuncional6() {
+		assertEquals(4, c.adecuacionFuncional(99,95,96));
+	}
+	
+	@Test
+	public void testAdecuacionFuncional7() {
+		assertEquals(-1, c.adecuacionFuncional(152,125,113));
+	}
+	
+	//TESTS DE MANTENIBILIDAD
+	@Test
+	public void testMantenibilidad() {
+		assertEquals(2, c.mantenibilidad(55,55,55,55,55));
+	}
+	
+	@Test
+	public void testMantenibilidad2() {
+		assertEquals(-1, c.mantenibilidad(-5,-5,-5, -2, -3));	
+	}
+	
+	@Test
+	public void testMantenibilidad3() {
+		assertEquals(0, c.mantenibilidad(7,8,5, 3, 9));	
+	}
+	
+	@Test
+	public void testMantenibilidad4() {
+		assertEquals(0, c.mantenibilidad(12,12,12,12,12));	
+	}
+	
+	@Test
+	public void testMantenibilidad5() {
+		assertEquals(1, c.mantenibilidad(38,38,38,38,38));
+	}
+	
+	@Test
+	public void testMantenibilidad6() {
+		assertEquals(1, c.mantenibilidad(40,40,40,40,40));
+	}
+	
+	@Test
+	public void testMantenibilidad7() {
+		assertEquals(3, c.mantenibilidad(75,75,75, 75, 75));
+		
+	}
+	
+	@Test
+	public void testMantenibilidad8() {
+		assertEquals(4, c.mantenibilidad(99,95,96, 92, 94));
+	}
+	
+	@Test
+	public void testMantenibilidad9() {
+		assertEquals(-1, c.mantenibilidad(152,125,113, 150, 168));
+	}
+
+	// TEST DE CALIDAD GLOBAL	
+	@Test
+	public void testCalidadGlobal() {
+		assertEquals(-1, c.calidadGlobal(0, 0));
+	}
+	
+	@Test
+	public void testCalidadGlobal2() {
+		assertEquals(1, c.calidadGlobal(5, 1));
+	}
+	
+	@Test
+	public void testCalidadGlobal3() {
+		assertEquals(1, c.calidadGlobal(1, 2));
+	}
+	
+	@Test
+	public void testCalidadGlobal4() {
+		assertEquals(2, c.calidadGlobal(5, 2));
+	}
+	
+	@Test
+	public void testCalidadGlobal5() {
+		assertEquals(2, c.calidadGlobal(1, 3));
+	}
+	
+	@Test
+	public void testCalidadGlobal6() {
+		assertEquals(2, c.calidadGlobal(2, 3));
+	}
+
+	@Test
+	public void testCalidadGlobal7() {
+		assertEquals(3, c.calidadGlobal(5, 3));
+	}
+
+	@Test
+	public void testCalidadGlobal8() {
+		assertEquals(3, c.calidadGlobal(4, 4));
+	}
+	
+	@Test
+	public void testCalidadGlobal9() {
+		assertEquals(4, c.calidadGlobal(5, 4));
+	}
+	
+	@Test
+	public void testCalidadGlobal10() {
+		assertEquals(3, c.calidadGlobal(1, 5));
+	}
+	
+	@Test
+	public void testCalidadGlobal11() {
+		assertEquals(3, c.calidadGlobal(2, 5));
+	}
+	
+	@Test
+	public void testCalidadGlobal12() {
+		assertEquals(4, c.calidadGlobal(3, 5));
+	}
+	
+	@Test
+	public void testCalidadGlobal13() {
+		assertEquals(4, c.calidadGlobal(4, 5));
+	}
+	
+	@Test
+	public void testCalidadGlobal14() {
+		assertEquals(5, c.calidadGlobal(5, 5));
+	}
+	
+	@Test
+	public void testCalidadGlobal15() {
+		assertEquals(-1, c.calidadGlobal(6, 5));
+	}
+	
+	@Test
+	public void testCalidadGlobal16() {
+		assertEquals(-1, c.calidadGlobal(5, 6));
+	}
+	
+	@Test
+	public void testCalidadGlobal17() {
+		assertEquals(-1, c.calidadGlobal(0, 4));
+	}
+	
+	@Test
+	public void testMain() {
+		m.main(null);
+	}
+	
+	
+	
+}
